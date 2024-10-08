f0209604c1599bb76513d2742c6a8f90569a342b
mistral04 <maciej.gatlik2@microsoft.wsei.edu.pl>
2024-10-08 13:18:45 +0200

    komunikaty

--- Laboratorium 1/Controllers/HomeController.cs
+++ Laboratorium 1/Controllers/HomeController.cs
@@ -8,6 +8,11 @@ namespace Laboratorium_1.Controllers
     {
         private readonly ILogger<HomeController> _logger;
 
+        public IActionResult About()
+        {
+            return View();
+        }
+
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
@@ -20,6 +25,44 @@ namespace Laboratorium_1.Controllers
 
         public IActionResult Privacy()
         {
+            return View();
+        }
+
+        public IActionResult Calculator(string op, double? a, double? b)
+        {
+            if(a is null || b is null)
+            {
+                ViewBag.ErrorMessage = "Niepoprawny format liczby w parametrze a lub b";
+                return View("CustomError");
+            }
+            
+            if (!(op == "add" || op == "sub" || op == "mul" || op == "div"))
+            {
+                ViewBag.ErrorMessage = "Niepoprawny format operacji";
+                return View("CustomError");
+            }
+            ViewBag.a = a;
+            ViewBag.b = b;
+            switch(op)
+            {
+                case "add":
+                    ViewBag.result = a + b;
+                    @ViewBag.op = '+';
+                    break;
+                case "sub":
+                    ViewBag.result = a - b;
+                    @ViewBag.op = '-';
+                    break;
+                case "mul":
+                    ViewBag.result = a * b;
+                    @ViewBag.op = '*';
+                    break;
+                case "div":
+                    ViewBag.result = a / b;
+                    @ViewBag.op = '÷';
+                    break;
+
+            }
             return View();
         }
 
